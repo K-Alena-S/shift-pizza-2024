47ed61197f2a05dc678834f4319d0b49a5ff8e38
K-Alena-S <a.krivonogova@g.nsu.ru>
2024-07-09 21:34:46 +0700

    Удалён лишний код

--- app/src/main/java/com/example/shift_pizza_2024/details/ContentComponent.kt
+++ app/src/main/java/com/example/shift_pizza_2024/details/ContentComponent.kt
@@ -65,25 +65,6 @@ private fun formatAmountText(ingredients: List<PizzaIngredient>): String {
 			Ingredient.SHRIMPS -> "Креветки"
 			Ingredient.CHICKEN_FILLET -> "Куриное филе"
 			Ingredient.MEATBALLS -> "Фрикадельки"
-//			Ingredient.PINEAPPLE -> R.string.ingredient_pineapple
-//			Ingredient.MOZZARELLA -> R.string.ingredient_mozzarella
-//			Ingredient.PEPERONI -> R.string.ingredient_peperoni
-//			Ingredient.GREEN_PEPPER -> R.string.ingredient_green_pepper
-//			Ingredient.MUSHROOMS -> R.string.ingredient_mushrooms
-//			Ingredient.BASIL -> R.string.ingredient_basil
-//			Ingredient.CHEDDAR -> R.string.ingredient_cheddar
-//			Ingredient.PARMESAN -> R.string.ingredient_parmesan
-//			Ingredient.FETA -> R.string.ingredient_feta
-//			Ingredient.HAM -> R.string.ingredient_ham
-//			Ingredient.PICKLE -> R.string.ingredient_pickle
-//			Ingredient.TOMATO -> R.string.ingredient_tomato
-//			Ingredient.BACON -> R.string.ingredient_bacon
-//			Ingredient.ONION -> R.string.ingredient_onion
-//			Ingredient.CHILE -> R.string.ingredient_chile
-//			Ingredient.SHRIMPS -> R.string.ingredient_shrimps
-//			Ingredient.CHICKEN_FILLET -> R.string.ingredient_chicken_fillet
-//			Ingredient.MEATBALLS -> R.string.ingredient_meatballs
-//			else -> ingredient.name.toString()
 		}
 		"$name: ${ingredient.cost}₽"
 	}
