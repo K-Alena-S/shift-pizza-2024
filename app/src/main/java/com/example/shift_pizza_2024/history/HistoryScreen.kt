8d89e84471139c7ae626ffc73cea6c87c76939ec
K-Alena-S <a.krivonogova@g.nsu.ru>
2024-07-10 16:27:17 +0700

    Удалён лишний код

--- app/src/main/java/com/example/shift_pizza_2024/history/HistoryScreen.kt
+++ app/src/main/java/com/example/shift_pizza_2024/history/HistoryScreen.kt
@@ -10,28 +10,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.shift_pizza_2024.R
-import com.example.shift_pizza_2024.network.PizzaRepository
-import kotlinx.coroutines.CancellationException
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
 
 @Composable
 fun HistoryScreen(
     historyViewModel: HistoryViewModel,
     onItemSelected: (loanId: Long) -> Unit,
 ) {
-    val scope = rememberCoroutineScope()
-//    var historyState by remember { mutableStateOf<HistoryState>(HistoryState.Initial) }
-
     val historyState by historyViewModel.state.collectAsState()
 
     LaunchedEffect(Unit) {
-//        loadLoans(repository, setHistoryState = { historyState = it })
         historyViewModel.loadPizzas()
     }
 
@@ -57,24 +48,6 @@ fun HistoryScreen(
                 pizza = state.pizzas,
                 onItemClicked = onItemSelected,
             )
-        }
-    }
-}
-
-private fun CoroutineScope.loadPizzas(
-    repository: PizzaRepository,
-    setHistoryState: (HistoryState) -> Unit,
-) {
-    launch {
-        setHistoryState(HistoryState.Loading)
-
-        try {
-            val pizzas = repository.getCatalog()
-            setHistoryState(HistoryState.Content(pizzas))
-        } catch (ce: CancellationException) {
-            throw ce
-        } catch (ex: Exception) {
-            setHistoryState(HistoryState.Failure(ex.message))
         }
     }
 }
