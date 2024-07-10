977cd62f65a995d8d7e94c97aa8ac3ac2b33cf91
K-Alena-S <a.krivonogova@g.nsu.ru>
2024-07-10 17:02:01 +0700

    Добавлены картинки пицц

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,4 +88,6 @@ dependencies {
     // Превью для Compose
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation("io.coil-kt:coil-compose:1.4.0")
 }
--- app/src/main/java/com/example/shift_pizza_2024/history/ContentComponent.kt
+++ app/src/main/java/com/example/shift_pizza_2024/history/ContentComponent.kt
@@ -1,19 +1,23 @@
 package com.example.shift_pizza_2024.history
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.example.shift_pizza_2024.data.Pizza
+import coil.compose.rememberImagePainter
 
 @Composable
 fun ContentComponent(
@@ -43,6 +47,24 @@ private fun PizzaItem(
     ) {
         Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
             Text(text = item.name)
+            LoadImageFromUrl(url = "https://shift-backend.onrender.com" + item.img)
         }
     }
+}
+
+@Composable
+fun LoadImageFromUrl(url: String) {
+    Image(
+        painter = rememberImagePainter(
+            data = url,
+            builder = {
+                crossfade(true)
+            }
+        ),
+        contentDescription = "Картинка пиццы",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp),
+        contentScale = ContentScale.Crop
+    )
 }
