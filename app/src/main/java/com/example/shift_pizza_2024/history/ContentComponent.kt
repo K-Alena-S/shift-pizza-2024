273686b5eaf98faf6b6fd724bcd598c441c61d7b
K-Alena-S <a.krivonogova@g.nsu.ru>
2024-07-10 16:33:05 +0700

    Добавлены отступы

--- app/src/main/java/com/example/shift_pizza_2024/history/ContentComponent.kt
+++ app/src/main/java/com/example/shift_pizza_2024/history/ContentComponent.kt
@@ -6,11 +6,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.shift_pizza_2024.data.Pizza
 
 @Composable
@@ -37,6 +39,7 @@ private fun PizzaItem(
         Modifier
             .fillMaxWidth()
             .clickable(onClick = onItemClicked)
+            .padding(vertical = 8.dp, horizontal = 16.dp),
     ) {
         Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
             Text(text = item.name)
