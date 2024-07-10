50d9804d010c4f398b452ed9f157d7c5f7084040
K-Alena-S <a.krivonogova@g.nsu.ru>
2024-07-10 20:26:39 +0700

    Добавлены все подклассы data класса CreatePizzaPaymentDto

---
+++ app/src/main/java/com/example/shift_pizza_2024/data/payment/DebitCard.kt
@@ -0,0 +1,7 @@
+package com.example.shift_pizza_2024.data.payment
+
+data class DebitCard(
+    val pan: String,
+    val exireDate: String,
+    val cvv: String
+)
---
+++ app/src/main/java/com/example/shift_pizza_2024/data/payment/Person.kt
@@ -0,0 +1,8 @@
+package com.example.shift_pizza_2024.data.payment
+
+data class Person(
+    val firstname: String,
+    val lastname: String,
+    val middlename: String,
+    val phone: String
+)
